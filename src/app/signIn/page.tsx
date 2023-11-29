2232413911c041ae94166f7d70efcd2f9debfec4
Faisal <officialfaisal1112@gmail.com>
2023-11-29 23:29:40 +0600

    design sign in page

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,71 +1,65 @@
+import Link from "next/link";
+
 const SignInPage = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-50 text-gray-800">
-        <div className="mb-8 text-center">
-          <h1 className="my-3 text-4xl font-bold">Sign in</h1>
-          <p className="text-sm text-gray-600">
-            Sign in to access your account
+    <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center p-10 min-h-screen space-y-12">
+      <div className="flex flex-col justify-center mx-auto w-full pb-12 md:pb-0 px-4 md:px-10 lg:px-20 border-[#81d8d0] border-b-[1px] md:border-b-0 md:border-r-[1px] ">
+        <div className="mb-8 text-center md:text-left">
+          <h1 className="my-1 text-2xl lg:text-3xl">Sign in</h1>
+          <p className="text-sm font-light text-gray-600">
+            Please sign in to your Timeless Account
           </p>
         </div>
-        <form action="" className="space-y-12">
-          <div className="space-y-4">
-            <div>
-              <label htmlFor="email" className="block mb-2 text-sm">
-                Email address
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="leroy@jenkins.com"
-                className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800"
-              />
-            </div>
-            <div>
-              <div className="flex justify-between mb-2">
-                <label htmlFor="password" className="text-sm">
-                  Password
-                </label>
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
-                  className="text-xs hover:underline text-gray-600"
-                >
-                  Forgot password?
-                </a>
-              </div>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="*****"
-                className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800"
-              />
-            </div>
+        <form action="" className="space-y-10">
+          <div>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              className="w-full p-1 border-black border-b-[1px] outline-none"
+            />
           </div>
+          <div>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              className="w-full p-1 border-black border-b-[1px] outline-none"
+            />
+          </div>
+
           <div className="space-y-2">
             <div>
               <button
                 type="button"
-                className="w-full px-8 py-3 font-semibold rounded-md bg-blue-600 text-gray-50"
+                className="w-full px-8 py-3 font-semibold bg-black hover:bg-[#81d8d0] text-white hover:text-black transition-all duration-300"
               >
                 Sign in
               </button>
             </div>
-            <p className="px-6 text-sm text-center text-gray-600">
-              Do not have an account yet?
-              <a
-                rel="noopener noreferrer"
-                href="#"
-                className="hover:underline text-blue-600"
-              >
-                Sign up
-              </a>
-              .
+            <p className="pt-4 text-xs hover:underline underline-offset-4 cursor-pointer">
+              Forgot your password?
             </p>
           </div>
         </form>
+      </div>
+
+      <div className="px-4 md:px-12 lg:px-20 text-center md:text-left min-h-[250px]">
+        <h1 className="text-2xl lg:text-3xl">Create an Account</h1>
+        <p className="text-sm font-light my-2">
+          Save time during checkout, view your shopping bag and saved items from
+          any device and access your order history.
+        </p>
+        <Link href={"/signUp"}>
+          <button
+            type="button"
+            className="w-full px-8 py-3 mt-4 font-semibold bg-black hover:bg-[#81d8d0] text-white hover:text-black transition-all duration-300"
+          >
+            Sign Up
+          </button>
+        </Link>
       </div>
     </div>
   );
