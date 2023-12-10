6786d77505a01b6a5b7726e1b9f5f0014fc8e105
Faisal <officialfaisal1112@gmail.com>
2023-12-10 23:14:36 +0600

    setup product api in redux

---
+++ src/redux/features/product/productApi.ts
@@ -0,0 +1,21 @@
+import { baseApi } from "@/redux/api/baseApi";
+
+const productApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    getAllProducts: build.query({
+      query: (storeId) => ({
+        url: `products/${storeId}`,
+        method: "GET",
+      }),
+    }),
+
+    getSingleProduct: build.query({
+      query: (id) => ({
+        url: `products/single-product/${id}`,
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const { useGetAllProductsQuery, useGetSingleProductQuery } = productApi;
