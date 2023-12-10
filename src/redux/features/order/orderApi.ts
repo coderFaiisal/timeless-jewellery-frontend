604e6969c00655a853e96d1583b018073058da5c
Faisal <officialfaisal1112@gmail.com>
2023-12-10 23:23:01 +0600

    setup order api into redux

---
+++ src/redux/features/order/orderApi.ts
@@ -0,0 +1,50 @@
+import { baseApi } from "@/redux/api/baseApi";
+
+const orderApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    createOrder: build.mutation({
+      query: (orderData) => ({
+        url: "orders/create-order",
+        method: "POST",
+        data: orderData,
+      }),
+    }),
+
+    getAllOrders: build.query({
+      query: (storeId) => ({
+        url: `orders/${storeId}`,
+        method: "GET",
+      }),
+    }),
+
+    getSingleOrder: build.query({
+      query: (id) => ({
+        url: `orders/single-order/${id}`,
+        method: "GET",
+      }),
+    }),
+
+    updateOrder: build.mutation({
+      query: ({ id, updatedData }) => ({
+        url: `orders/${id}`,
+        method: "PATCH",
+        data: updatedData,
+      }),
+    }),
+
+    deleteOrder: build.mutation({
+      query: (id) => ({
+        url: `orders/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useCreateOrderMutation,
+  useGetAllOrdersQuery,
+  useGetSingleOrderQuery,
+  useUpdateOrderMutation,
+  useDeleteOrderMutation,
+} = orderApi;
