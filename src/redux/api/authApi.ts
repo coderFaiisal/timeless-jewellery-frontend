6d02ae3de04a2c0f64b28fa9fdcfb95c508adab6
Faisal <officialfaisal1112@gmail.com>
2023-12-10 23:10:08 +0600

    setup user api in redux

--- src/redux/api/authApi.ts
+++ src/redux/api/authApi.ts
@@ -12,7 +12,7 @@ export const authApi = baseApi.injectEndpoints({
 
     userSignIn: build.mutation({
       query: (signInData) => ({
-        url: "",
+        url: "auth/sign-in",
         method: "POST",
         data: signInData,
       }),
@@ -20,7 +20,7 @@ export const authApi = baseApi.injectEndpoints({
 
     changePassword: build.mutation({
       query: (passwordData) => ({
-        url: "",
+        url: "auth/change-password",
         method: "POST",
         data: passwordData,
       }),
---
+++ src/redux/features/user/userApi.ts
@@ -0,0 +1,33 @@
+import { baseApi } from "@/redux/api/baseApi";
+
+const userApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    getUserProfile: build.query({
+      query: () => ({
+        url: "users/my-profile",
+        method: "GET",
+      }),
+    }),
+
+    updateUserProfile: build.mutation({
+      query: (updatedData) => ({
+        url: "users/my-profile",
+        method: "PATCH",
+        data: updatedData,
+      }),
+    }),
+
+    deleteUserProfile: build.mutation({
+      query: (id) => ({
+        url: `users/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetUserProfileQuery,
+  useUpdateUserProfileMutation,
+  useDeleteUserProfileMutation,
+} = userApi;
