c8b55cc9688d33427bcb19194482d1575097dbf7
Faisal <officialfaisal1112@gmail.com>
2024-01-04 21:33:17 +0600

    subscribe cart

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,7 +1,9 @@
 import { baseApi } from "./api/baseApi";
+import cartReducer from "./features/cart/cartSlice";
 import previewModalReducer from "./features/productPreview/productPreviewSlice";
 
 export const reducer = {
+  cart: cartReducer,
   previewModal: previewModalReducer,
   [baseApi.reducerPath]: baseApi.reducer,
 };
